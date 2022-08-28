959ed31cfcf674fe13995f0364d6f4cfb4d12137
Justice <ujustice@quidax.com>
2022-08-28 11:04:09 +0100

    fix(talentql): 🐛 test to see github actions

--- src/app.ts
+++ src/app.ts
@@ -17,121 +17,121 @@ type TPaging = {
 };
 
 
-const startApp = async () => {
 
- let currentPage = 1;
- let tableData: TResultData[] = [];
- let dataStore: TpaginationData = {};
- let status: "LOADING" | "DATA" | "ERROR" | "IDLE" = "IDLE";
+let currentPage = 1;
+let tableData: TResultData[] = [];
+let dataStore: TpaginationData = {};
+let loadStatus: "LOADING" | "DATA" | "ERROR" | "IDLE" = "IDLE";
 
- let dataSink: HTMLTableColElement | null = document.querySelector('[ data-sink]');
- let dataLoading: HTMLParagraphElement | null = document.querySelector('[ data-loading]');
- let btnGroup = document.querySelectorAll(".btn-group");
- let btnGroupEl: HTMLElement | null = document.querySelector(".btn-group");
+let dataSink: HTMLTableColElement | null = document.querySelector('[ data-sink]');
+let dataLoading: HTMLParagraphElement | null = document.querySelector('[ data-loading]');
+let btnGroup = document.querySelectorAll(".btn-group");
+let btnGroupEl: HTMLElement | null = document.querySelector(".btn-group");
 
- async function fetchData(page: number): Promise<ResultResponse> {
+async function fetchData(page: number): Promise<ResultResponse> {
 
-  status = "LOADING";
-  renderData();
-  let url = `https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84&page=${page}`
-  const response = await fetch(url);
-  if (!response.ok) {
-   status = "ERROR"
-   renderData()
-   const message = `An error has occured: ${response.status}`;
-   throw new Error(message);
-  }
-  status = "DATA"
-  const result = await response.json();
-  return result;
+ loadStatus = "LOADING";
+ renderData();
+ let url = `https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84&page=${page}`
+ const response = await fetch(url);
+ if (!response.ok) {
+  loadStatus = "ERROR"
+  renderData()
+  const message = `An error has occured: ${response.status}`;
+  throw new Error(message);
  }
+ loadStatus = "DATA"
+ const result = await response.json();
+ return result;
+}
 
 
- async function getData(page: number): Promise<void> {
-  //return the data from cache
-  if (dataStore[page]) {
+async function getData(page: number): Promise<void> {
+ //return the data from cache
+ if (dataStore[page]) {
+  tableData = dataStore[page]
+  currentPage = page
+  renderData()
+ } else {
+  fetchData(page).then(data => {
+   //the api result returns the page number and page number + 1 as keys, we would like to cache it so that we dont make such round trip again
+   delete data?.results[0]?.paging
+   Object.assign(dataStore, data?.results[0]);
    tableData = dataStore[page]
    currentPage = page
    renderData()
-  } else {
-   fetchData(page).then(data => {
-    //the api result returns the page number and page number + 1 as keys, we would like to cache it so that we dont make such round trip again
-    delete data?.results[0]?.paging
-    Object.assign(dataStore, data?.results[0]);
-    tableData = dataStore[page]
-    currentPage = page
-    renderData()
-   });
-  }
+  });
  }
+}
 
- const renderData = () => {
-  let newHtml
+const renderData = () => {
+ let newHtml
 
 
-  if (status == "DATA") {
-   newHtml = tableData.map(data => {
-    const { id, row, age, gender } = data
-    return `
+ if (loadStatus == "DATA") {
+  newHtml = tableData.map(data => {
+   const { id, row, age, gender } = data
+   return `
         <tr data-entryid="${id}">
         <td>${row}</td>
         <td>${age}</td>
         <td>${gender}</td>
         </tr>
         `
-   })
-   if (dataLoading) dataLoading.style.visibility = "hidden"
+  })
+  if (dataLoading) dataLoading.style.visibility = "hidden"
 
-   if (pageView && dataSink) {
-    pageView.textContent = `page ${currentPage}`
-    dataSink.innerHTML = newHtml.join("").toString()
-   }
-
-   if (currentPage === 1) {
-    previousButton.disabled = true
-    return
-   } else {
-    previousButton.disabled = false
-   }
+  if (pageView && dataSink) {
+   pageView.textContent = `Showing Page ${currentPage}`
+   dataSink.innerHTML = newHtml.join("").toString()
   }
 
-  if (status === "ERROR" && dataSink) {
-   newHtml = `
+  if (currentPage === 1) {
+   previousButton.disabled = true
+   return
+  } else {
+   previousButton.disabled = false
+  }
+ }
+
+ if (loadStatus === "ERROR" && dataSink) {
+  newHtml = `
             <p>Error getting data</p>
             <button id="retryBtn">retry</button>
             `
-   dataSink.innerHTML = newHtml.toString()
-   const retryBtn: HTMLButtonElement | null = document.querySelector("#retryBtn")
-   retryBtn?.addEventListener("click", () => {
-    getData(currentPage)
-   });
+  dataSink.innerHTML = newHtml.toString()
+  const retryBtn: HTMLButtonElement | null = document.querySelector("#retryBtn")
+  retryBtn?.addEventListener("click", () => {
+   getData(currentPage)
+  });
 
-  }
-
-  if (status === "LOADING" && dataLoading) {
-   dataLoading.style.visibility = "visible"
-  }
  }
 
-
- const goToNextPage = () => {
-  getData(currentPage + 1)
+ if (loadStatus === "LOADING" && dataLoading) {
+  dataLoading.style.visibility = "visible"
  }
-
- const goToPreviousPage = () => {
-  getData(currentPage - 1)
- }
+}
 
 
+const goToNextPage = () => {
+ getData(currentPage + 1)
+}
 
- const [previous, next, pageView] = btnGroup[0].children
- const previousButton = previous as HTMLButtonElement
- const nextButton = next as HTMLButtonElement
+const goToPreviousPage = () => {
+ getData(currentPage - 1)
+}
 
- previousButton.addEventListener("click", goToPreviousPage)
- nextButton.addEventListener("click", goToNextPage)
 
- getData(currentPage)
+
+const [previous, next, pageView] = btnGroup[0].children
+const previousButton = previous as HTMLButtonElement
+const nextButton = next as HTMLButtonElement
+
+previousButton.addEventListener("click", goToPreviousPage)
+nextButton.addEventListener("click", goToNextPage)
+
+getData(currentPage)
+const startApp = async () => {
 };
 
 document.addEventListener('DOMContentLoaded', startApp);
