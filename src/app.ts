b9464bfd4d3cfb1fdb5322a75db1a6f243f2bc7e
Justice <ujustice@quidax.com>
2022-09-14 11:46:00 +0100

    swap gender with age row

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
 interface ResultResponse {
- results: {
-  [key: number]: TResultData[];
-  paging?: TPaging;
- }[];
+      results: {
+            [key: number]: TResultData[];
+            paging?: TPaging;
+      }[];
 }
 type TResultData = {
- id: string;
- row: number;
- age: number;
- gender: string;
+      id: string;
+      row: number;
+      age: number;
+      gender: string;
 };
 type TpaginationData = Record<number, TResultData[]>
 type TPaging = {
- next: string;
- previous?: string;
+      next: string;
+      previous?: string;
 };
 
 
@@ -30,95 +30,95 @@ let btnGroupEl: HTMLElement | null = document.querySelector(".btn-group");
 
 async function fetchData(page: number): Promise<ResultResponse> {
 
- loadStatus = "LOADING";
- renderData();
- let url = `https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84&page=${page}`
- const response = await fetch(url);
- if (!response.ok) {
-  loadStatus = "ERROR"
-  renderData()
-  const message = `An error has occured: ${response.status}`;
-  throw new Error(message);
- }
- loadStatus = "DATA"
- const result = await response.json();
- return result;
+      loadStatus = "LOADING";
+      renderData();
+      let url = `https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84&page=${page}`
+      const response = await fetch(url);
+      if (!response.ok) {
+            loadStatus = "ERROR"
+            renderData()
+            const message = `An error has occured: ${response.status}`;
+            throw new Error(message);
+      }
+      loadStatus = "DATA"
+      const result = await response.json();
+      return result;
 }
 
 
 async function getData(page: number): Promise<void> {
- //return the data from cache
- if (dataStore[page]) {
-  tableData = dataStore[page]
-  currentPage = page
-  renderData()
- } else {
-  fetchData(page).then(data => {
-   //the api result returns the page number and page number + 1 as keys, we would like to cache it so that we dont make such round trip again
-   delete data?.results[0]?.paging
-   Object.assign(dataStore, data?.results[0]);
-   tableData = dataStore[page]
-   currentPage = page
-   renderData()
-  });
- }
+      //return the data from cache
+      if (dataStore[page]) {
+            tableData = dataStore[page]
+            currentPage = page
+            renderData()
+      } else {
+            fetchData(page).then(data => {
+                  //the api result returns the page number and page number + 1 as keys, we would like to cache it so that we dont make such round trip again
+                  delete data?.results[0]?.paging
+                  Object.assign(dataStore, data?.results[0]);
+                  tableData = dataStore[page]
+                  currentPage = page
+                  renderData()
+            });
+      }
 }
 
 const renderData = () => {
- let newHtml
+      let newHtml
 
 
- if (loadStatus == "DATA") {
-  newHtml = tableData.map(data => {
-   const { id, row, age, gender } = data
-   return `
+      if (loadStatus == "DATA") {
+            newHtml = tableData.map(data => {
+                  const { id, row, age, gender } = data
+                  return `
         <tr data-entryid="${id}">
         <td>${row}</td>
-        <td>${age}</td>
         <td>${gender}</td>
+        <td>${age}</td>
         </tr>
         `
-  })
-  if (dataLoading) dataLoading.style.visibility = "hidden"
+            })
+            if (dataLoading) dataLoading.style.visibility = "hidden"
 
-  if (pageView && dataSink) {
-   pageView.textContent = `Showing Page ${currentPage}`
-   dataSink.innerHTML = newHtml.join("").toString()
-  }
+            if (pageView && dataSink) {
+                  pageView.textContent = `Showing Page ${currentPage}`
+                  dataSink.innerHTML = newHtml.join("").toString()
+            }
 
-  if (currentPage === 1) {
-   previousButton.disabled = true
-   return
-  } else {
-   previousButton.disabled = false
-  }
- }
+            if (currentPage === 1) {
+                  previousButton.disabled = true
+                  return
+            } else {
+                  previousButton.disabled = false
+            }
+      }
 
- if (loadStatus === "ERROR" && dataSink) {
-  newHtml = `
+      if (loadStatus === "ERROR" && dataSink) {
+            newHtml = `
             <p>Error getting data</p>
             <button id="retryBtn">retry</button>
             `
-  dataSink.innerHTML = newHtml.toString()
-  const retryBtn: HTMLButtonElement | null = document.querySelector("#retryBtn")
-  retryBtn?.addEventListener("click", () => {
-   getData(currentPage)
-  });
+            dataSink.innerHTML = newHtml.toString()
+            const retryBtn: HTMLButtonElement | null = document.querySelector("#retryBtn")
+            retryBtn?.addEventListener("click", () => {
+                  getData(currentPage)
+            });
 
- }
+      }
 
- if (loadStatus === "LOADING" && dataLoading) {
-  dataLoading.style.visibility = "visible"
- }
+      if (loadStatus === "LOADING" && dataLoading) {
+            dataLoading.style.visibility = "visible"
+      }
 }
 
 
 const goToNextPage = () => {
- getData(currentPage + 1)
+      getData(currentPage + 1)
 }
 
 const goToPreviousPage = () => {
- getData(currentPage - 1)
+      getData(currentPage - 1)
 }
 
 
